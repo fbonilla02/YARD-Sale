ffc1d426b8455d07c424ce31b0bbc726644b40ce
francisco bonilla <franciscobonillac@gmail.com>
2022-10-06 18:22:56 -0500

    arreglamos los botones del navbar

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const cartIcon = document.querySelector('.navbar-shopping-cart')
+const shopingCartDrop = document.querySelector('.product-detail')
 
-menuEmail.addEventListener('click', ()=>{   
+
+menuEmail.addEventListener('click', ()=>{
+    const isAsideClosed = shopingCartDrop.classList.contains('inactive')
+    if(!isAsideClosed){
+        shopingCartDrop.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
 
+})
+
+burguerIcon.addEventListener('click', ()=>{
+    const isAsideClosed = shopingCartDrop.classList.contains('inactive')
+
+    if(!isAsideClosed){
+        shopingCartDrop.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+})
+
+cartIcon.addEventListener('click', ()=>{
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    shopingCartDrop.classList.toggle('inactive')
 })
